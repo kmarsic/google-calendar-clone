dc708d86dc3056f4a6c133d640a45af646b8d131
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-14 22:16:27 +0100

    updated post method and added input fields

--- frontend/src/Components/Tasks/NewTaskForm.jsx
+++ frontend/src/Components/Tasks/NewTaskForm.jsx
@@ -3,15 +3,28 @@ import { useState } from "react";
 import { nanoid } from "@reduxjs/toolkit";
 import './../../styles/newTaskForm.scss';
 
-export function NewTaskForm () {
+export function NewTaskForm ({render}) {
 
-    const [inputValue, setInputValue] = useState("");
+    const [formData, setFormData] = useState({
+        list: "My Tasks",
+        ID: nanoid(),
+        type: "task",
+        updatedAt: "",
+        location : "",
+        time: "",
+        description: "",
+        color: "#008000",
+        title: "",
+        completed: false
+    })
 
-    const onInputChange = e => setInputValue(e.target.value);
+    const handleInputChange = (e) => {
+        setFormData({...formData, [e.target.id]: e.target.value})
+    }
 
     const updateData = async (updatedData) => {
         try {
-            const response = await fetch(`http://localhost:9000/tasks`, {
+            const response = await fetch(`http://localhost:9000/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -32,36 +45,89 @@ export function NewTaskForm () {
     }
 
     function handleSubmit(e) {
-        if (e.type === "submit") {
-                e.preventDefault();
-            }
-        if (inputValue === "") return;
-        const updatedData = { 
-            id: nanoid(),
-            title: inputValue,
-            completed: false
-            }
-        updateData(updatedData)
+        e.preventDefault();
+        if (!formData.title) return;
+        updateData(formData);
+        setFormData({
+            list: "My Tasks",
+            ID: nanoid(),
+            type: "task",
+            updatedAt: "",
+            time: "",
+            location: "",
+            description: "",
+            color: "#008000",
+            title: ""
+          });
+        render()
         
-        setInputValue("");
-        }
+    }
     
 
     return(
     <div className="event-add">
         <form onSubmit={handleSubmit} className="form">
             <div>
-                <input type="text"
-                id="new-input"
+                <input 
+                type="text"
+                id="title"
+                required
                 placeholder="Create a new task..."
-                value={inputValue}
-                onChange={onInputChange}
+                value={formData.title}
+                onChange={(e) => handleInputChange(e)}
                 />
             </div>
             <div className="event-type">
                 <div><span>Event</span></div>
-                <div>Task</div>
+                <div><span>Task</span></div>,
+                <button style={{display: "none"}}></button>
             </div>
+            <div className="date-time">
+                <div>
+                    <input 
+                    type="text"
+                    id="time" 
+                    placeholder="Date and time"
+                    value={formData.time}
+                    onChange={(e) => handleInputChange(e)}>
+                    </input>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <input 
+                    type="text"
+                    id="location"
+                    placeholder="Location"
+                    value={formData.location}
+                    onChange={(e) => handleInputChange(e)}>
+                    </input>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <input 
+                    type="text"
+                    id="description"
+                    placeholder="Description"
+                    value={formData.description}
+                    onChange={(e) => handleInputChange(e)}
+                    >
+                    </input>
+                </div>
+            </div>
+            <hr />
+            <div>
+                <div>
+                    <label htmlFor="color">Pick a color </label>
+                    <input
+                    id="color"
+                    type="color"
+                    value={formData.color}
+                    onChange={(e) => handleInputChange(e)}
+                     />
+                </div>
+            </div> 
         
         </form>
     </div>
