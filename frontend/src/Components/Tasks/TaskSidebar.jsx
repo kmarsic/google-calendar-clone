feb6d78f8ae7ed849e02dc9f60b24a9b21028ff3
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-14 22:16:48 +0100

    removed unused functions

--- frontend/src/Components/Tasks/TaskSidebar.jsx
+++ frontend/src/Components/Tasks/TaskSidebar.jsx
@@ -1,25 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import './../../styles/taskSidebar.scss'
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { allTasks } from '../../redux/features/taskSlicer';
-import getData from '../../redux/features/thunk/getData';
 
 export const TaskSidebar = ({burgerOpen}) => {
     const fetchedData = useSelector(allTasks);
-    const [serverData, setServerData] = useState(fetchedData)
-    const dispatch = useDispatch();
-     useEffect(() => {
-    dispatch(getData());
-    setServerData(fetchedData)
-    }, [burgerOpen])
     return (
         <div className={burgerOpen ? 'open sidebar' : 'closed sidebar'}>
             <ul className='taskList'>
-                {serverData.data.map(user => {
+                {fetchedData.data.map(user => {
                     return(
-                        <li key={user.id}>{user.title}</li>
+                        <li key={user._id}>{user.title}</li>
                     )
                 })}
             </ul>
