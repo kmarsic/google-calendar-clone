27bacd5ca69dae813f7615ea5a8f730a9eeba9b0
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-14 21:02:49 +0100

    updated api route

--- frontend/src/redux/features/thunk/getData.js
+++ frontend/src/redux/features/thunk/getData.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const getData = createAsyncThunk("taskManager/getData", async () => {
     try {
-        const response = await fetch("http://localhost:9000/tasks");
+        const response = await fetch("http://localhost:9000");
         if (!response.ok) {
             throw new Error("Failed to fetch data");
         }
