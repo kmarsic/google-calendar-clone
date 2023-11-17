499f59faca03e159e361442098ee6e884d575ea2
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-17 20:42:10 +0100

    updated schema

--- backend/db/tasksdb.js
+++ backend/db/tasksdb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
     list: String,
-    ID: String,
+    name: Number,
     type: String,
     createdAt: {
         type: Date,
