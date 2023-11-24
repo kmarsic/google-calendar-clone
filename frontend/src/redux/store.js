10c56e3208eaaad816e4831e6f90ceb96d61b289
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-24 17:43:25 +0100

    added click to state

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import dateReducer from './features/dateSlicer';
 import taskReducer from './features/taskSlicer';
+import clickReducer from './features/clickedSlicer';
 
 
 export const store = configureStore({
     reducer: {
         dateList: dateReducer,
         taskList: taskReducer,
+        clicked: clickReducer,
     }
 })
