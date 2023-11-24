fa3336b37982ba16953580ee826107d4c16ce042
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-24 17:41:58 +0100

    removed today function

--- frontend/src/redux/features/dateSlicer.js
+++ frontend/src/redux/features/dateSlicer.js
@@ -3,27 +3,37 @@ import { createSlice } from "@reduxjs/toolkit";
 export const dateManager = createSlice({
     name:'dateManager',
     initialState: {
-        selectedDate: Date.parse(new Date()),
+        mainDate: Date.parse(new Date()),
+        miniDate: Date.parse(new Date()),
         today: {
             dayIndex: "",
             dayString: ""
         },
+        
     },
     reducers: {
-        nextMonth(state) {
-            const date = new Date(state.selectedDate)
+        nextMonth(state,action) {
+            const date = new Date(state.mainDate)
             const nextMonth = date.getMonth() + 1;
             const nextYear = date.getFullYear();
             const newDate = new Date(nextYear, nextMonth);
-            state.selectedDate = Date.parse(newDate);
+            if (action.payload == "mini") {
+                state.miniDate = Date.parse(newDate);
+                return;
+            }
+            state.mainDate = Date.parse(newDate);
         },
         
-        previousMonth(state) {
-            const date = new Date(state.selectedDate)
+        previousMonth(state,action) {
+            const date = new Date(state.mainDate)
             const prevMonth = date.getMonth() - 1;
             const prevYear = date.getFullYear();
             const newDate= new Date(prevYear, prevMonth);
-            state.selectedDate = Date.parse(newDate)
+            if (action.payload == "mini") {
+                state.miniDate = Date.parse(newDate);
+                return;
+            }
+            state.mainDate = Date.parse(newDate)
        },
        setToday(state, action) {
         state.today = action.payload
@@ -33,7 +43,8 @@ export const dateManager = createSlice({
 
 export const {nextMonth, previousMonth, setToday} = dateManager.actions;
 
-export const currentDate = (state) => state.dateList.selectedDate;
+export const currentDate = (state) => state.dateList.mainDate;
+export const miniDate = (state) => state.dateList.miniDate;
 export const currentDay = (state) => state.dateList.today;
 
 export default dateManager.reducer;
