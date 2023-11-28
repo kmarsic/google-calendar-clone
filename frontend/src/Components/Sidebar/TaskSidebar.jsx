0712a17961897f1b4b92d0ed0ce85e6bbd202124
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-28 02:58:59 +0100

    added mini calendar

--- frontend/src/Components/Sidebar/TaskSidebar.jsx
+++ frontend/src/Components/Sidebar/TaskSidebar.jsx
@@ -5,7 +5,7 @@ import { MiniMonthView } from './../MonthView/mini/MiniMonthView';
 export const TaskSidebar = ({burgerOpen}) => {
     return (
         <div className={burgerOpen ? 'open sidebar' : 'closed sidebar'}>
-            {/* <MiniMonthView/> */}
+            <MiniMonthView/>
         </div>
     )
 }
