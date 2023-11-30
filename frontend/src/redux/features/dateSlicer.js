ca849e6740f8bdb8e6cbd9e143e04e8a6053e62f
Kristijan Maršić <kmarsic99@gmail.com>
2023-11-30 11:47:26 +0100

    mini date changes with main date

--- frontend/src/redux/features/dateSlicer.js
+++ frontend/src/redux/features/dateSlicer.js
@@ -12,36 +12,44 @@ export const dateManager = createSlice({
         
     },
     reducers: {
-        nextMonth(state,action) {
+        nextMonth(state) {
             const date = new Date(state.mainDate)
             const nextMonth = date.getMonth() + 1;
             const nextYear = date.getFullYear();
             const newDate = new Date(nextYear, nextMonth);
-            if (action.payload == "mini") {
-                state.miniDate = Date.parse(newDate);
-                return;
-            }
+            state.miniDate = Date.parse(newDate);
             state.mainDate = Date.parse(newDate);
         },
+        nextMonthMini(state) {
+            const date = new Date(state.miniDate)
+            const nextMonth = date.getMonth() + 1;
+            const nextYear = date.getFullYear();
+            const newDate = new Date(nextYear, nextMonth);
+            state.miniDate = Date.parse(newDate);
+        },
         
-        previousMonth(state,action) {
+        previousMonth(state) {
             const date = new Date(state.mainDate)
             const prevMonth = date.getMonth() - 1;
             const prevYear = date.getFullYear();
             const newDate= new Date(prevYear, prevMonth);
-            if (action.payload == "mini") {
-                state.miniDate = Date.parse(newDate);
-                return;
-            }
-            state.mainDate = Date.parse(newDate)
+            state.miniDate = Date.parse(newDate);
+            state.mainDate = Date.parse(newDate);
        },
+        previousMonthMini(state) {
+            const date = new Date(state.miniDate)
+            const prevMonth = date.getMonth() - 1;
+            const prevYear = date.getFullYear();
+            const newDate= new Date(prevYear, prevMonth);
+            state.miniDate = Date.parse(newDate)
+        },
        setToday(state, action) {
         state.today = action.payload
        }
     }
 })
 
-export const {nextMonth, previousMonth, setToday} = dateManager.actions;
+export const {nextMonth, previousMonth, setToday, previousMonthMini, nextMonthMini} = dateManager.actions;
 
 export const currentDate = (state) => state.dateList.mainDate;
 export const miniDate = (state) => state.dateList.miniDate;
