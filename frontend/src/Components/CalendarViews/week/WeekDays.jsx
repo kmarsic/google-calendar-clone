a670520d62cb987bb221250f1300b072615de54d
Kristijan Maršić <kmarsic99@gmail.com>
2023-12-05 23:41:03 +0100

    added styles for current date

--- frontend/src/Components/CalendarViews/week/WeekDays.jsx
+++ frontend/src/Components/CalendarViews/week/WeekDays.jsx
@@ -2,13 +2,19 @@
 import { Link } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { setDate } from "../../../redux/features/dateSlicer";
-export function WeekDays({date,day,today}) {
+export function WeekDays({date,day}) {
     const dispatch = useDispatch();
+    const newDate = new Date(date);
+    const todayDate = new Date();
+    let isToday = false;
+    if (todayDate.getMonth() == newDate.getMonth() && todayDate.getFullYear() == newDate.getFullYear() && todayDate.getDate() == newDate.getDate()){
+        isToday = true;
+    }
     return (
         <div className="week-day">
-            <div className="week-day-name">{day}</div>
+            <div className={isToday ? "week-day-name wname-today" : "week-day-name"}>{day}</div>
             <Link to='/day'>
-                <div onClick={() => dispatch(setDate(Date.parse(date)))} id={Date.parse(date)}className={today ? "week-day-index week-today" : "week-day-index"}>
+                <div onClick={() => dispatch(setDate(Date.parse(date)))} id={Date.parse(date)} className={isToday ? "week-day-index windex-today" : "week-day-index"}>
                     <span>{date.getDate()}</span>
                 </div>
             </Link>
