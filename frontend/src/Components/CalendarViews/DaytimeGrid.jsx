7a2376b7f46c6e719f027d652f5e21b4c38d5bbb
Kristijan Maršić <kmarsic99@gmail.com>
2023-12-05 02:38:49 +0100

    structure

--- frontend/src/Components/CalendarViews/DaytimeGrid.jsx
+++ frontend/src/Components/CalendarViews/DaytimeGrid.jsx
@@ -1,17 +1,19 @@
-export function DaytimeGrid() {
+/* eslint-disable react/prop-types */
+export function DaytimeGrid({iterator}) {
     const dayGrid = () => {
         const grid = [];
         for (let i = 0; i < 24; i++) {
+            
             grid.push(
-                <div className="time-slot"></div>
+                <div key={"ts" + i} className={iterator == 0 ? "hairline-border" : "time-slot"}></div>
             )
         }
 
         return grid;
     }
     return (
-        <>
+        <div className={iterator == 0 ? "hairline" : "week-column"}>
         {dayGrid()}
-        </>
+        </div>
     )
 }
