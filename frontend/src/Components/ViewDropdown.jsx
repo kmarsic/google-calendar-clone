904decd4f5dd5ccf5e1c20c003dd7f1eedfc8a73
Kristijan Maršić <kmarsic99@gmail.com>
2023-12-07 15:47:01 +0100

    reducers updated

--- frontend/src/Components/ViewDropdown.jsx
+++ frontend/src/Components/ViewDropdown.jsx
@@ -1,38 +1,40 @@
 import { Link } from "react-router-dom";
 import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { setView } from "../redux/features/dateSlicer";
 export function ViewDropdown () {
     const dropdownRef = useRef(null);
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        // Timeout to ensure the component is in the DOM before adding the class
         const timeoutId = setTimeout(() => {
           dropdownRef.current.classList.add('visible');
         }, 10);
     
-        // Clear the timeout on component unmount
         return () => clearTimeout(timeoutId);
       }, []);
     return (
         <>
             <ul ref={dropdownRef} className="dropdown">
-                    <li>
+                    <li onClick={() => dispatch(setView("Day"))}>
                         <Link to={"/day"}>
                             <div>Day</div>
                             <div>D</div>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={() => dispatch(setView("Week"))}>
                         <Link to={"/week"}>
                             <div>Week</div>
                             <div>W</div>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={() => dispatch(setView("Month"))}>
                         <Link to={"/month"}>
                             <div>Month</div>
                             <div>M</div>
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={() => dispatch(setView("Year"))}>
                         <Link to={"/year"}>
                             <div>Year</div>
                             <div>Y</div>
