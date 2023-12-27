9c21a6d0363759fd9b4ca1d1f0c34582c953cd5b
Kristijan Maršić <kmarsic99@gmail.com>
2023-12-27 10:43:05 +0100

    basename to /

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename='/'>
     <Provider store={store}>
-      <BrowserRouter basename='/calendar'>
         <App />
-      </BrowserRouter>
     </Provider>
+  </BrowserRouter>
 
 )
