c1bce6571b1007f82c4d72da8566a8300ce748bf
Kristijan Maršić <kmarsic99@gmail.com>
2024-02-24 17:39:12 +0100

    form state change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,31 +6,22 @@ import { Navigation } from './Components/Nav/Navigation';
 import { Sidebar } from './Components/Sidebar/Sidebar';
 import { Helmet } from 'react-helmet'
 import { SiteRouter } from './SiteRouter';
-//funcs
+import { NewTaskForm } from './Components/Forms/NewTaskForm';
+//deps
 import { currentDate } from './redux/features/dateSlicer';
 import getData from './redux/features/thunk/getData';
 import { useDispatch, useSelector } from 'react-redux'
-import { useState, useEffect, useRef } from 'react';
-import { NewTaskForm } from './Components/Forms/NewTaskForm';
+import { useState, useEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   const [burger, setBurgerOpen] = useState(false);
-  const [clickedElement, setClickedElement] = useState({
-    id: null,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    cursorX: 0,
-    cursorY: 0,
-    date: null,
-    clicked : false
-  });
+  const [clickedElement, setClickedElement] = useState(null);
 
   const dispatch = useDispatch()
 
   const date = new Date(useSelector(currentDate))
   const today = new Date().getDate();
-
-  const formRef = useRef(null);
 
   useEffect(() => {
     dispatch(getData());
@@ -40,37 +31,18 @@ function App() {
     return `https://calendar.google.com/googlecalendar/images/favicons_2020q4/calendar_${num}.ico`
   }
 
-  useEffect(() => {
-    const handleClick = (e) => {
-      if (e.target.classList.contains('box') || e.target.classList.contains('hours')) {
-        setClickedElement({
-          ...clickedElement,
-          id: e.target.id,
-          cursorX: e.clientX,
-          cursorY: e.clientY,
-          date: new Date(parseInt(e.target.id)),
-          clicked: true
-        })
-      }
-    }; 
-    
-    const handleClickOutside = (e) => {
-      if (formRef.current && !formRef.current.contains(e.target)) {
-        setClickedElement(
-          {
-            ...clickedElement,
-            clicked: false
-          }
-        );
-      }
-    } 
-    document.body.addEventListener('click', handleClick);
-    document.addEventListener('mousedown', handleClickOutside);
+  const handleClick = (e) => {
 
-    return () => {
-      document.body.removeEventListener('click', handleClick);
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    if (e.target.classList.contains('box') || e.target.classList.contains('hours')) {
+      setClickedElement({
+        id: e.target.id,
+        data: e,
+      })
+      return;
+  }}
+  useEffect(() => {
+    document.body.addEventListener('click', handleClick);
+    return ( () => document.body.removeEventListener('click'))
   }, []);
 
   return (
@@ -79,22 +51,23 @@ function App() {
       <title>Google Calendar - {date.toLocaleString("default", {month: "long", year: 'numeric' })}</title>
       <link rel="icon" type="image/x-icon" href={faviconHref(today)}></link>
     </Helmet>
-    <Navigation today={today} burger={burger} setBurger={setBurgerOpen}/>
-    <div className='calendar-main'>
-      {clickedElement.clicked && (
-        <div ref={formRef}>
-          <NewTaskForm
-          clickedElement={clickedElement}
-          onClose={() => setClickedElement({
-            ...clickedElement,
-            date: null,
-            clicked: false
-          })}/>
-        </div>
-      )}
-      <Sidebar burgerOpen={burger}/>
-      <SiteRouter/>
-    </div>
+    <Navigation burger={burger} setBurger={setBurgerOpen} setElement={setClickedElement}/>
+    <AnimatePresence>
+      <div className='calendar-main'>
+        <AnimatePresence>
+          {clickedElement && (
+            <>
+              <NewTaskForm
+              clickedElement={clickedElement}
+              onClose={() => setClickedElement(null)}/>
+              <div className="overlay" onClick={() => setClickedElement(null)}></div>
+            </>
+          )}
+        </AnimatePresence>
+        <Sidebar burgerOpen={burger}/>
+        <SiteRouter/>
+      </div>
+    </AnimatePresence>
   </>
   )
 }
