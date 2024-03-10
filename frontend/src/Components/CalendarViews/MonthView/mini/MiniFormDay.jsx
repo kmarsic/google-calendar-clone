7f80d7483fd721d1525474c41584257327e324b2
kmarsic <kmarsic99@gmail.com>
2024-03-10 15:17:21 +0100

    state change on click

--- frontend/src/Components/CalendarViews/MonthView/mini/MiniFormDay.jsx
+++ frontend/src/Components/CalendarViews/MonthView/mini/MiniFormDay.jsx
@@ -1,23 +1,29 @@
 /* eslint-disable react/prop-types */
-import { useSelector } from "react-redux";
-import { focusDate } from "../../../../redux/features/dateSlicer";
+import { useDispatch, useSelector } from "react-redux";
+import { focusDate, setDate, setFocusDate } from "../../../../redux/features/dateSlicer";
 import { useContext } from "react";
 import { EventChangeContext, TimeContext } from "../../../Forms/formContext";
+import { FocusContext } from "../../../Forms/Inputs/InputTime";
 
 export function MiniFormDay({date, iterator, previous, next, isToday}) {
+    const focus = useSelector(focusDate);
+    const dispatch = useDispatch();
+
     const formTime = useContext(TimeContext);
     const dispatchReducer = useContext(EventChangeContext);
-    const handleDateChange = (time, date) => {
-        dispatchReducer({type: time, payload: new Date(date)})
-    }
-    const parsedDate = Date.parse(date);
+    const resetFocus = useContext(FocusContext);
 
-    const focus = useSelector(focusDate);
+    const handleDateChange = (time, date) => {
+        dispatchReducer({type: time, payload: date})
+    }
+
+
+    const parsedDate = Date.parse(date);
     return (
         <div 
         id={parsedDate}
         name={formTime}
-        onClick={() => handleDateChange(formTime, parsedDate)}
+        onClick={() => {handleDateChange(formTime, date); dispatch(setFocusDate(parsedDate)); dispatch(setDate(parsedDate)); resetFocus(false) }}
         className="mini-box">
             <div className={`${isToday ? "day-index-mini mini-today" : "day-index-mini"} ${parsedDate == focus ? "focused-date" : null} ${previous || next ? "empty" : null}`}><span>{iterator}</span></div>
         </div>
