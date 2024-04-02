eeb3bda7caa2afec42cb6076107d0214474f5ea8
kmarsic <kmarsic99@gmail.com>
2024-04-02 10:00:51 +0200

    modals and overflow control

--- frontend/src/Components/Forms/Assignment.jsx
+++ frontend/src/Components/Forms/Assignment.jsx
@@ -1,25 +1,113 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import removeData from "../../redux/features/thunk/removeData";
-import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { allTasks } from "../../redux/features/taskSlicer";
 import { removeTask } from "../../redux/features/taskSlicer";
+import { setDate, setView } from "../../redux/features/dateSlicer";
+import { useEffect, useState } from "react";
 
-export function Assignment({title,task,color}) {
+export function Assignment({date}) {
     const dispatch = useDispatch()
+    const assignments = useSelector(allTasks);
 
-    const handleSubmit = (e) => {
+    const [overflowVisible, setOverflowVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleSubmit = (e,task) => {
         e.preventDefault();
         if (e.target.children[0].innerText == "???") return
         dispatch(removeData(task));
         dispatch(removeTask(task.ID));
     }
+
+    function mapAssignments(list) {
+        const filtered = list.filter((task) => {
+            return task.name == date
+        })
+        const mapped = filtered.map((task, index) => {
+            return <Task key={index} handleSubmit={handleSubmit} task={task}/>
+        })
+        return mapped
+    }
+
+    useEffect(() => {
+        const filtered = assignments.filter((task) => {
+            return task.name == date
+        })
+        if (filtered.length < 4) {
+            setOverflowVisible(false);
+        } else if (filtered.length > 4) {
+            setOverflowVisible(true)
+        }
+    }, [assignments])
+
     return (
-        <form onSubmit={(e) => handleSubmit(e)} className='assignment' style={{backgroundColor: color}}>
-            <p>{title}</p>
-            <button>
-                <FontAwesomeIcon icon={faXmarkCircle} size="xl" color="#ffffff"/>
-            </button>
+        <>
+            {overflowVisible ? 
+            <Overflow list={assignments} date={date} handleSubmit={handleSubmit} setOverflowVisible={setOverflowVisible} setModalVisible={setModalVisible}/> 
+            : 
+            mapAssignments(assignments)}
+            {modalVisible ? 
+            <OverflowModal list={assignments} setModalVisible={setModalVisible} date={date} handleSubmit={handleSubmit}/> 
+            : null}
+        </>
+    )
+}
+
+function Task({handleSubmit, task}) {
+    return (
+        <form onSubmit={(e) => handleSubmit(e, task)} className='assignment' style={{backgroundColor: task.color}}>
+                <p>{task.title}</p>
         </form>
+    )
+}
+
+function Overflow({list, date, handleSubmit, setModalVisible}) {
+    const filtered = list.filter((task) => {
+        return task.name == date
+    })
+    const mappedList = filtered.map((task,index) => {
+        if (index > 3) {
+            return
+        } else return <Task key={index} handleSubmit={handleSubmit} task={task}/>
+    })
+    return (
+        <>
+            {mappedList}
+            <div onClick={() => setModalVisible(true)} className="assignment-overflow">{mappedList.length - 4} more</div>
+        </>
+    )
+}
+
+function OverflowModal({list, setModalVisible, handleSubmit, date}) {
+    const dispatch = useDispatch()
+    const day = new Date(date).toLocaleString("default", {weekday: "short"}).toUpperCase();
+    const newDate = new Date(date).getDate();
+    const filtered = list.filter((task) => {
+        return task.name == date
+    })
+    const mapped = filtered.map((task, index) => {
+        return <Task key={index} handleSubmit={handleSubmit} task={task}/>
+    })
+    return (
+        <>
+            <div className="assignment-overflow-modal-overlay" onClick={() => setModalVisible(false)}></div>
+            <div className="assignment-overflow-modal">
+                <div className="modal-div">
+                    <div>{day}</div>
+                    <div>
+                        <span onClick={() => {dispatch(setDate(date)); dispatch(setView("Day"))}}>{newDate}</span>
+                    </div>
+                    <FontAwesomeIcon className="modal-exit" icon={faXmark} onClick={() => setModalVisible(false)}/>
+                </div>
+                <div>
+                    {mapped}
+                </div>
+            </div>
+        </>
+
     )
 }
