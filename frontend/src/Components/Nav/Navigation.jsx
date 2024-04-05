cee1d8300a8962a584f9fe5c2907b8e40d121c61
kmarsic <kmarsic99@gmail.com>
2024-04-05 11:28:53 +0200

    removed useEffect breaking url and view state

--- frontend/src/Components/Nav/Navigation.jsx
+++ frontend/src/Components/Nav/Navigation.jsx
@@ -1,24 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-//hooks
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-//components,state
-import { currentView,} from '../../redux/features/dateSlicer';
 import { Hamburger } from './Hamburger';
 import { NavDateControl } from './NavDateControl';
 import { NavView } from './NavView';
 
 export const Navigation = ({ burger, setBurger}) => {
-  const view = useSelector(currentView);
-  const navigate = useNavigate();
 
-  // sync route with current view
-  useEffect(() => {
-    navigate(`/${view}`);
-  }, [view]);
-  
     return (
         <div className="calendar-header">
           <Hamburger burger={burger} setBurger={setBurger}></Hamburger>
