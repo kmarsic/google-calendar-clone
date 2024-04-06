34fa39034abdf648e73dc65ed0f59c65635c3ed3
kmarsic <kmarsic99@gmail.com>
2024-04-06 18:45:22 +0200

    removed unnecessary state

--- frontend/src/Components/Forms/FormModules/EventType.jsx
+++ frontend/src/Components/Forms/FormModules/EventType.jsx
@@ -1,16 +1,30 @@
 /* eslint-disable react/prop-types */
-export function EventType({setFormType}) {
+import { useContext } from "react";
+import { EventForm } from "./EventForm";
+import { TaskForm } from "./TaskForm";
+import { EventChangeContext, EventDataContext } from "../formContext";
+
+export function EventType() {
+    const formData = useContext(EventDataContext);
+    const dispatchReducer = useContext(EventChangeContext);
+    const handleEventTypeChange = (typ) => {
+        dispatchReducer({type: "type", payload: typ})
+    }
+
     return (
+    <>
         <div className="event-type">
-            <button id="event" onClick={(e) => {e.preventDefault(); setFormType(true)}}>
+            <button id="event" value="event" onClick={(e) => {e.preventDefault(); handleEventTypeChange("event")}}>
                 <span>Event</span>
             </button>
-
-            <button id="task" onClick={(e) => {e.preventDefault();setFormType(false)}}>    
+            <button id="task" onClick={(e) => {e.preventDefault();handleEventTypeChange("task")}}>    
                 <span>Task</span>
-            </button>
-            
+            </button>            
             <button style={{ display: "none" }}></button>
         </div>
+        {formData.type === "event" ? <EventForm/> : <TaskForm/>}
+    </>
+        
+
     )
 }
