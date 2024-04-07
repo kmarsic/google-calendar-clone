23df1caca21de15f3d03a2c1ccc2a32b211ac58f
kmarsic <kmarsic99@gmail.com>
2024-04-07 18:26:46 +0200

    moved focus context

---
+++ frontend/src/Components/Forms/formContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const EventDataContext = createContext(null);
+export const EventChangeContext = createContext(null);
+export const TaskDataContext = createContext(null);
+export const TaskChangeContext = createContext(null);
+export const TimeContext = createContext(null);
+export const FocusContext = createContext(null);
