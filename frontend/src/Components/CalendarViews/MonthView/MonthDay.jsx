071eb5db9e50595b3b8ae489cfc2dd035e492675
kmarsic <kmarsic99@gmail.com>
2024-04-28 19:50:30 +0200

    removed redundant conditions

--- frontend/src/Components/CalendarViews/MonthView/MonthDay.jsx
+++ frontend/src/Components/CalendarViews/MonthView/MonthDay.jsx
@@ -13,26 +13,12 @@ export function MonthDay({date, iterator,isToday, previous, next}) {
         id={parsedDate} 
         className={previous || next ? "box empty" : "box"}
         >
-          {previous ? 
-          (<Link to='/Day'><span onClick={() => {dispatch(setDate(parsedDate)); dispatch(setView("Day"))}} className='dayIndex'>{iterator}</span></Link>) : 
-          iterator === 1 ? 
-          (
-            <> 
-              {isToday ? null : date.toLocaleString("default", {month:"short"}) + " "}
-              <Link to='/Day'>
-                <span key={"ff" + iterator} onClick={() => {dispatch(setDate(parsedDate)); dispatch(setView("Day"))}} className={`dayIndex ${isToday ? "today" : ""}`}>
-                  {iterator}
-                </span>
-              </Link>
-            </>
-          ) : 
-            <Link to='/Day'>
-              <span onClick={() => {dispatch(setDate(parsedDate)); dispatch(setView("Day"))}} className={`dayIndex ${isToday ? "today" : ""}`}>
+          {iterator === 1 ? date.toLocaleString("default", {month:"short"}) + " " : null}
+          <Link to='/Day'>
+            <span key={"ff" + iterator} onClick={() => dispatch(setDate(parsedDate))} className={`dayIndex ${isToday ? "today" : ""}`}>
                 {iterator}
-              </span>
-            </Link>
-            }
-          <Assignment date={parsedDate}/>
+            </span>
+          </Link>
         </div>
     )
 }
