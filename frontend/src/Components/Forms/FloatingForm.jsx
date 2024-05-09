5b6f0cc967f3274b5c82ce28adef909967c9fad9
kmarsic <kmarsic99@gmail.com>
2024-05-09 11:46:02 +0200

    moved dropdown function

--- frontend/src/Components/Forms/FloatingForm.jsx
+++ frontend/src/Components/Forms/FloatingForm.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
-import { FormDropdown } from "./FormDropdown";
+import { motion } from 'framer-motion';
+import { createPortal } from 'react-dom';
 
 export function FloatingForm({ burger, handleClick }) {
     const [visible, setVisible] = useState(false);
@@ -32,4 +33,23 @@ export function FloatingForm({ burger, handleClick }) {
         <FormDropdown handleClick={handleClick} isOpen={visible} onClose={() => setVisible(null)}/>
         </>
     )
+}
+
+function FormDropdown({ isOpen, onClose, handleClick }) {
+
+    if (isOpen) {
+        return (
+        <motion.div
+        className='form-dropdown'
+        initial={{opacity: 0, scale: 0.8, top: "80px", left: "25px", originY: "100px", originX: 0}}
+        animate={{opacity: 1, scale: 1, top: "145px", left: "25px"}}
+        transition={{duration: 0.1}}
+        exit={{opacity: 0}}>
+            <ul>
+                <li id='form-event' onClick={(e) => {handleClick(e); onClose()}}>Event</li>
+                <li id='form-task' onClick={(e) => {handleClick(e); onClose()}}>Task</li>
+            </ul>
+            {createPortal(<div className='overlay' onClick={onClose}></div>, document.body)}
+        </motion.div>
+    )} else return null
 }
