c4873d9c063df2fdb15ddaa548e09a62b21eff4d
kmarsic <kmarsic99@gmail.com>
2024-06-06 15:44:00 +0200

    button styles

--- frontend/src/Components/Forms/FormModules/FormDock.jsx
+++ frontend/src/Components/Forms/FormModules/FormDock.jsx
@@ -5,12 +5,8 @@ import { faGripLines, faXmark } from "@fortawesome/free-solid-svg-icons"
 export function FormDock({startDrag, onClose}) {
     return (
         <div className="form-dock" onMouseDown={startDrag}>
-                <button className="btn-round btn-effect">
-                    <FontAwesomeIcon icon={faGripLines} color="var(--text-body)" size="lg"/>
-                </button>
-                <button className="btn-round btn-effect" onClick={() => onClose()}>
-                    <FontAwesomeIcon color="var(--text-body)" icon={faXmark} size="lg"/>
-                </button>
+                <FontAwesomeIcon icon={faGripLines} color="var(--text-body)" size="lg" className="btn-round btn-effect"/>
+                <FontAwesomeIcon onClick={onClose} color="var(--text-body)" icon={faXmark} size="lg" className="btn-round btn-effect"/>
         </div>
     )
 }
