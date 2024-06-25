05c7b7513329e0b45e14dedec5e8f2a14af57f3e
kmarsic <kmarsic99@gmail.com>
2024-06-25 23:09:11 +0200

    local to cloud db

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,54 @@
-const createError = require('http-errors');
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require("cors");
-const app = express();
+const {MongoClient, ServerApiVersion} = require('mongodb');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); 
- 
-const taskRouter = require("./api/tasks")
+const uri = "mongodb+srv://test:vQQ4OIU6sNJliXJd@calendar-clone.pgdlqha.mongodb.net/?retryWrites=true&w=majority&appName=calendar-clone";
 
-app.use("/tasks", taskRouter)
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-module.exports = app;
+
+async function main() {
+
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1});
+
+        await createListing(client, {
+             name: "testuser",
+             events: [
+                "date1", "date2", "date3"
+             ],
+             dates: "exampledates",
+             tasks: "list tasks"
+        })
+        console.log("Pinged deployment. Nice")
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error)
+
+async function createListing(client, newUser) {
+    const result = await client.db("todo").collection("item").insertOne(newUser);
+
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function createMultipleListings(client, newListings) {
+    client.db("todo").collection("item").insertMany(newListings);
+}
+
+async function listDatabases() {
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => {
+        console.log(` - ${db.name}`)
+    })
+}
