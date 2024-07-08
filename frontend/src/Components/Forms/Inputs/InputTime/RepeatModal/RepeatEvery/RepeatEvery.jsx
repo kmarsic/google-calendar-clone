459a4a8bf5dc164644fc0bbbdb833f0ceed9f66b
kmarsic <kmarsic99@gmail.com>
2024-07-08 12:43:02 +0200

    struct

---
+++ frontend/src/Components/Forms/Inputs/InputTime/RepeatModal/RepeatEvery/RepeatEvery.jsx
@@ -0,0 +1,12 @@
+import {  RepeatNumberInput } from "./RepeatNumberInput";
+import { RepeatUnit } from "./RepeatUnit";
+
+export function RepeatEvery() {
+    return(
+        <div className="repeat-every">
+            <p>Repeat every</p>
+            <RepeatNumberInput/>
+            <RepeatUnit/>
+        </div>
+    )
+}
