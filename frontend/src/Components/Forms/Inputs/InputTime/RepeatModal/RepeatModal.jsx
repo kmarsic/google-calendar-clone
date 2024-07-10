f71e02ff5234863b9cbea5df66dbf6340fe164ac
kmarsic <kmarsic99@gmail.com>
2024-07-10 13:49:11 +0200

    modified data structure for repeat logic

--- frontend/src/Components/Forms/Inputs/InputTime/RepeatModal/RepeatModal.jsx
+++ frontend/src/Components/Forms/Inputs/InputTime/RepeatModal/RepeatModal.jsx
@@ -1,36 +1,31 @@
 import { createPortal } from "react-dom";
 import { RepeatOn } from "./RepeatOn/RepeatOn";
-import { RepeatEnds } from "./RepeatEnds";
+import { RepeatEnds } from "./RepeatEnds/RepeatEnds";
 import { RepeatFooter } from "./RepeatFooter";
 import { RepeatEvery } from "./RepeatEvery/RepeatEvery";
-import { useReducer } from "react";
+import { useContext, useReducer } from "react";
 import { createContext } from "react";
+import { EventDataContext } from "../../../formContext";
 
-export const RepeatStateContext = createContext(null);
-export const ChangeRepeatStateContext = createContext(null);
 export const RepeatDataContext = createContext(null);
 export const ChangeRepeatDataContext = createContext(null);
 
 export function RepeatModal({ onClose }) {
-  const [modalState, setModalState] = useReducer(stateReducer,{
-    every: "week",
-    ends: {
-      never: false,
-      on: false,
-      after: false
-    },
-  });
-
+  const formData = useContext(EventDataContext);
   const [modalData, setModalData] = useReducer(dataReducer, {
-    everyUnit: 1,
-    repeatOn: "",
-    endOn: ""
+    unit: "weeks",
+    repeatInterval: 1,
+    repeatOnWeekDays: [
+      new Date(formData.startDate).getDay(),
+    ],
+    repeatOnDayIndex : "",
+    repeatOnWeekDayOccurence: "",
+    endOn: "",
+    endAfterOccurences: 13
   });
 
   return createPortal(
     <>
-      <RepeatStateContext.Provider value={modalState}>
-        <ChangeRepeatStateContext.Provider value={setModalState}>
           <RepeatDataContext.Provider value={modalData}>
             <ChangeRepeatDataContext.Provider value={setModalData}>
             <div className="overlay dark" onClick={onClose}></div>
@@ -43,32 +38,48 @@ export function RepeatModal({ onClose }) {
             </div>
             </ChangeRepeatDataContext.Provider>
           </RepeatDataContext.Provider>
-        </ChangeRepeatStateContext.Provider>  
-      </RepeatStateContext.Provider>
     </>, document.body
   );
 }
 
-function stateReducer(state, action) {
-  switch (action.type) {
-    case "every":
-      return { ...state, every: action.payload };
-    case "ends":
-      return { ...state, ends: action.payload };
-      default:
-        return state;
-  }
-}
-
 function dataReducer(state, action) {
   switch (action.type) {
-    case "everyUnit":
-      return { ...state, everyUnit: action.payload };
-    case "repeatOn":
-      return { ...state, repeatOn: action.payload };
+    case "unit":
+      return {
+        ...state,
+        unit: action.payload,
+      };
+    case "repeatInterval":
+      return {
+        ...state,
+        repeatInterval: action.payload,
+      };
+    case "repeatOnWeekDays":
+      return {
+        ...state,
+        repeatOnWeekDays: action.payload,
+      };
+    case "repeatOnDayIndex":
+      return {
+        ...state,
+        repeatOnDayIndex: action.payload,
+      };
+    case "repeatOnWeekDayOccurence":
+      return {
+        ...state,
+        repeatOnWeekDayOccurence: action.payload,
+      };
     case "endOn":
-      return { ...state, endOn: action.payload };
-      default:
-        return state;
+      return {
+        ...state,
+        endOn: action.payload,
+      };
+    case "endAfterOccurences":
+      return {
+        ...state,
+        endAfterOccurences: action.payload,
+      };
+    default:
+      return state;
   }
 }
