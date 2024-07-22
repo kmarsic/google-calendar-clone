3371eada42111f782bea5a8b38df0e59a60f2e4b
kmarsic <kmarsic99@gmail.com>
2024-07-22 11:07:59 +0200

    update view on option click

--- frontend/src/Components/Nav/ViewDropdown.jsx
+++ frontend/src/Components/Nav/ViewDropdown.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { motion } from 'framer-motion';
+import { setView } from "../../redux/features/dateSlicer";
 
 export function ViewDropdown () {
+    const dispatch = useDispatch();
+
+    const handleClick = (view) => {
+        dispatch(setView(view))
+    }
     return (
     <motion.ul
         style={{x: "-172px", y: "110px", originX:"250px", originY: 0}}
@@ -12,25 +18,25 @@ export function ViewDropdown () {
         exit={{opacity: 0}}
         className="view-dropdown"
         >
-                        <li>
+                        <li onClick={() => handleClick("Day")}>
                             <Link to={"/Day"}>
                                 <div>Day</div>
                                 <div>D</div>
                             </Link>
                         </li>
-                        <li>
+                        <li onClick={() => handleClick("Week")}>
                             <Link to={"/Week"}>
                                 <div>Week</div>
                                 <div>W</div>
                             </Link>
                         </li>
-                        <li>
+                        <li onClick={() => handleClick("Month")}>
                             <Link to={"/Month"}>
                                 <div>Month</div>
                                 <div>M</div>
                             </Link>
                         </li>
-                        <li>
+                        <li onClick={() => handleClick("Year")}>
                             <Link to={"/Year"}>
                                 <div>Year</div>
                                 <div>Y</div>
