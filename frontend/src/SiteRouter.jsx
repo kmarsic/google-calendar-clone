7a76433015501ee9541c3cb2c8c3792bdce3d578
kmarsic <kmarsic99@gmail.com>
2024-07-22 11:48:56 +0200

    faulty url navigates to monthview

--- frontend/src/SiteRouter.jsx
+++ frontend/src/SiteRouter.jsx
@@ -1,13 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { MonthView } from "./Components/CalendarViews/MonthView/MonthView";
 import { YearView } from "./Components/CalendarViews/Year/YearView";
 import { WeekView } from "./Components/CalendarViews/week/WeekView";
 import { DayView } from "./Components/CalendarViews/day/DayView";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { currentDate } from "./redux/features/dateSlicer";
 
-export function SiteRouter() {   
+export function SiteRouter() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    useEffect(() => {
+        if (location.pathname === "/") {
+            navigate("/Month");
+        }
+    }, [])
+
+    useEffect(() => {}, [])
     return (
         <Routes>
-            <Route path="/" element={<MonthView />} />
+            <Route path="/" element={<MonthView />}/>
             <Route path="Month" element={<MonthView />} />
             <Route path="Year" element={<YearView />} />
             <Route path="Week" element={<WeekView />} />
