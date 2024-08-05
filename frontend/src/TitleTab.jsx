ef222a6602a6b45100cf9d871b26558909284345
kmarsic <kmarsic99@gmail.com>
2024-08-05 13:15:45 +0200

    view sensitive

--- frontend/src/TitleTab.jsx
+++ frontend/src/TitleTab.jsx
@@ -1,22 +1,55 @@
 import { Helmet } from "react-helmet";
 import { useSelector } from "react-redux";
-import { currentDate } from "./redux/features/dateSlicer";
+import { currentDate, currentView } from "./redux/features/dateSlicer";
 
 
 export function TitleTab() {
     const date = new Date(useSelector(currentDate));
     const today = new Date().getDate();
+    const view = useSelector(currentView);
 
     const faviconHref = (num) => `https://calendar.google.com/googlecalendar/images/favicons_2020q4/calendar_${num}.ico`;
+
+    const determineTitle = () => {
+        switch (view) {
+            case "Year":
+                return (
+                    `${date.toLocaleString("default", {
+                        year: "numeric",
+                    })} year`
+                )
+            case "Month":
+                return (
+                    date.toLocaleString("default", {
+                        month: "long",
+                        year: "numeric",
+                    })
+                );
+            case "Week":
+                const firstOfWeek = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
+                return (
+                    `Week of ${firstOfWeek.toLocaleString("default", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric"
+                    })}`
+                );
+            case "Day":
+                return (
+                    date.toLocaleString("default", {
+                        weekday: "long",
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric"
+                    })
+                )
+        }
+    }
 
     return (
         <Helmet>
                 <title>
-                    Google Calendar -{" "}
-                    {date.toLocaleString("default", {
-                        month: "long",
-                        year: "numeric",
-                    })}
+                    Google Calendar -{" "} {determineTitle()}
                 </title>
                 <link
                     rel="icon"
