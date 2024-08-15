a75b46fb5cd651a1a7b9ec6da3b43e2f8fcfe8d6
kmarsic <kmarsic99@gmail.com>
2024-08-15 12:47:00 +0200

    placeholder

---
+++ frontend/src/Components/Assignments/PlaceholderTask.jsx
@@ -0,0 +1,26 @@
+import checkmark from "./../../styles/icons/checkmark.png";
+import checkmarkDark from "./../../styles/icons/checkmark-dark.png";
+
+export function PlaceholderTask({task}) {
+    const defaultStyle = {
+        backgroundColor: task.allDay ? task.color : "transparent",
+        color: task.allDay ? "white" : "black",
+        left: 4,
+    }
+
+    const time = new Date(task.startTime);
+    console.log("im alive")
+    return (
+        <>
+        <div
+        className="assignment"
+        style={defaultStyle}
+        >
+        <span style={{backgroundColor: task.color}} className={task.allDay ? null : "assignment-allDay"}></span>
+        {task.allDay ? null : time.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+        {task.type === "form-event" ? null : task.allDay ? (<img src={checkmark} style={{width: "12px"}}/>) : (<img src={checkmarkDark} style={{width: "12px"}}/>)}
+        <span>{task.title == "" ? "(No title)" : task.title}</span>
+        </div>
+        </>
+    );
+}
